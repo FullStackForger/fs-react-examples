be9568664d7aea707c8066800a0e360c32bfbd77
IndieForger <indieforger@gmail.com>
2016-03-04 00:37:54 +0000

    component tree

---
+++ basic-06-component-tree/app.js
@@ -0,0 +1,29 @@
+const ChildComponent = React.createClass({
+	render: function () {
+		return (
+			<div className="panel panel-default">
+				<div className="panel-heading">Child component</div>
+				<div className="panel-body">
+					This is child no {this.props.childNumber}
+				</div>
+			</div>
+			)
+	}
+});
+
+const ParentComponent = React.createClass({
+	render: function () {
+		return (
+			<div className="panel panel-default">
+				<div className="panel-heading">Parent component</div>
+				<div className="panel-body">
+				<ChildComponent childNumber="1"/>
+					<ChildComponent childNumber="2"/>
+					<ChildComponent childNumber="3"/>
+				</div>
+			</div>
+		)
+	}
+});
+
+ReactDOM.render( <ParentComponent />, document.getElementById('app') );
