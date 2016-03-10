f299c253461866f0d4441721ece85175ecccb610
IndieForger <indieforger@gmail.com>
2016-03-10 01:37:15 +0000

    basic clic counter

---
+++ basic-06-simple-click-counter/app.js
@@ -0,0 +1,22 @@
+const Clicker = React.createClass({
+	getInitialState: function () {
+		return { count: 0 };
+	},
+	handleClick: function () {
+		this.setState({
+			count: this.state.count + 1,
+		});
+	},
+	render: function () {
+		return (
+			<button onClick={this.handleClick}>
+				Click me! Number of clicks: {this.state.count}
+			</button>
+		);
+	}
+})
+
+ReactDOM.render(
+	<Clicker label=""/>,
+	document.getElementById('app')
+);
