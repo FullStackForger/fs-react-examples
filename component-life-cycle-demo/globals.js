eef8cc8f119dcbbd6703abb0c02fbd812814cde0
IndieForger <indieforger@gmail.com>
2016-03-14 15:59:16 +0000

    code separation

---
+++ component-life-cycle-demo/globals.js
@@ -0,0 +1,24 @@
+window.highlight = (function() {
+	let highlightIds = [];
+	let highlightId;
+
+	setInterval(function () {
+		let el;
+		if (highlightId) {
+			el = document.getElementById(highlightId);
+			el.className = el.className.replace('alert-success', 'alert-info');
+			highlightId = null;
+		}
+
+		if (highlightIds.length > 0) {
+			highlightId = highlightIds.shift();
+			el = document.getElementById(highlightId);
+			el.className = el.className.replace('alert-info', 'alert-success');
+		}
+	}, 500);
+
+	return function (elementId) {
+		console.log(elementId);
+		highlightIds.push(elementId);
+	}
+})()
--- component-life-cycle-demo/suspect.js
+++ component-life-cycle-demo/suspect.js
@@ -1,27 +1,3 @@
-window.highlight = (function() {
-	let highlightIds = [];
-	let highlightId;
-
-	setInterval(function () {
-		let el;
-		if (highlightId) {
-			el = document.getElementById(highlightId);
-			el.className = el.className.replace('alert-success', 'alert-info');
-			highlightId = null;
-		}
-
-		if (highlightIds.length > 0) {
-			highlightId = highlightIds.shift();
-			el = document.getElementById(highlightId);
-			el.className = el.className.replace('alert-info', 'alert-success');
-		}
-	}, 500);
-
-	return function (elementId) {
-		console.log(elementId);
-		highlightIds.push(elementId);
-	}
-})()
 
 const Suspect = React.createClass({
 	// -------------------------------------
@@ -93,7 +69,5 @@ const Suspect = React.createClass({
 	}
 
 });
-
-
 
 ReactDOM.render( <Suspect />, document.getElementById('app') );
