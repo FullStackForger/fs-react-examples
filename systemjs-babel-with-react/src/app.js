bdf38a2fda4a6c94572591f4f2f339b3cadbb819
IndieForger <indieforger@gmail.com>
2016-07-22 09:30:14 +0100

    systemjs-babel-with-react/

---
+++ systemjs-babel-with-react/src/app.js
@@ -0,0 +1,22 @@
+import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+
+const Panel = React.createClass({
+	render: function () {
+		return (
+			<div className="panel panel-default">
+				<div className="panel-heading">{this.props.title}</div>
+				<div className="panel-body">
+					{this.props.content}
+				</div>
+			</div>
+		)
+	}
+})
+
+ReactDOM.render( (
+	<Panel
+		title="This is title"
+		content="This is some totally random content"
+	/>
+), document.getElementById('app') );
