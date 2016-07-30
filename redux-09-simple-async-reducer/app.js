388fc1542ae820b6ca86be38b52afa5876f2ab64
IndieForger <indieforger@gmail.com>
2016-07-30 23:17:45 +0100

    inline comments

--- redux-09-simple-async-reducer/app.js
+++ redux-09-simple-async-reducer/app.js
@@ -3,6 +3,7 @@ import thunk from 'redux-thunk'
 
 function counter(state = {}, action) {
 	console.log('action dispatched: ' + JSON.stringify(action))
+	// ... switch case
 	return state
 }
 
@@ -10,6 +11,10 @@ let middlewares = applyMiddleware(thunk)
 let store = createStore(counter, middlewares)
 
 store.dispatch((dispatch) => {
+
+	// we could make number of http requests sent to different endpoints
+	// then we would fire actions upon receiving response results at different times
+
 	setTimeout(() => dispatch({ type: 'RESPONSE_RESULT', action: 1 }), 300)
 	setTimeout(() => dispatch({ type: 'RESPONSE_RESULT', action: 2 }), 50)
 	setTimeout(() => dispatch({ type: 'RESPONSE_RESULT', action: 3 }), 500)
